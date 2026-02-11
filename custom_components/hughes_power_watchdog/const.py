50bb924fb2b883bf76bfec44c985511665bffb0a
jdaleo23 <153868428+jdaleo23@users.noreply.github.com>
2026-02-11 13:36:35 -0500

    Added Reset Energy command

--- custom_components/hughes_power_watchdog/const.py
+++ custom_components/hughes_power_watchdog/const.py
@@ -2,13 +2,9 @@
 
 DOMAIN = "hughes_power_watchdog"
 
-# The UUIDs we found
-SERVICE_UUID = "0000ff01-0000-1000-8000-00805f9b34fb"
 CHARACTERISTIC_UUID = "0000ff01-0000-1000-8000-00805f9b34fb"
-
-# The Magic Handshake we discovered
-# "!%!%,protocol,open,"
 HANDSHAKE_PAYLOAD = bytes.fromhex("212521252c70726f746f636f6c2c6f70656e2c")
 
+CMD_RESET_ENERGY = "2479774001060300007121"
 
 CONF_DEVICE_NAME = "device_name"
